98867e95b1e541d787d20ebee86ab734d8047950
lucmarbay <lukimartinezbayon@gmail.com>
2022-11-27 15:54:17 +0100

    Creacion del proyecto

---
+++ src/main/java/com/PruebaMerc/PruebaMerc/PruebaMercApplication.java
@@ -0,0 +1,13 @@
+package com.PruebaMerc.PruebaMerc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaMercApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaMercApplication.class, args);
+	}
+
+}
